e84fb07e2012aa698d0ce1b7ecdac90ac98a1c13
cahyadipg007 <cahyadipg007@gmail.com>
2019-01-26 16:02:40 -0800

    Update naha.js

--- naha.js
+++ naha.js
@@ -18,7 +18,8 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://linkalternatif.formatguru.com/2017/08/cad-software-2d-and-3d-computer-aided.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://linkalternatif.formatguru.com/2019/01/penjelasan-singkat-mengenai-autocad.html
+?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
